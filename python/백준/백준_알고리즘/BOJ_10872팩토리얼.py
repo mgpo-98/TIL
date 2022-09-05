4b6d9ab2b7bc6b40eb593b1d978b7910acd9e1a6
mgpo-98 <csi4613@naver.com>
2022-09-05 22:34:24 +0900

    0905

---
+++ python/백준/백준_알고리즘/BOJ_10872팩토리얼.py
@@ -0,0 +1,8 @@
+n = int(input())
+k = 1
+p = 1
+if n > 0:
+    for i in range(1,n+1):
+        k *= i
+    print(k)
+else : print(k)
---
+++ python/백준/백준_알고리즘/BOJ_5988홀수짝수.py
@@ -0,0 +1,7 @@
+t = int(input())
+for i in range(t):
+    a = int(input())
+    if a % 2 == 0:
+        print('even')
+    else:
+        print('odd')
