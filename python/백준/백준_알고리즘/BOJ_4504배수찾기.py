c55de48db4106398faf432ab466c04da4c1c9e17
mgpo-98 <csi4613@naver.com>
2022-10-17 21:52:08 +0900

    1017

---
+++ python/백준/백준_알고리즘/BOJ_4504배수찾기.py
@@ -0,0 +1,10 @@
+n = int(input())
+while True:
+    a = int(input())
+    if a == 0 :
+        break
+    elif a % n == 0:
+        print(f'{a} is a multiple of {n}.')
+    else :
+        print(f'{a} is NOT a multiple of {n}.')
+    
