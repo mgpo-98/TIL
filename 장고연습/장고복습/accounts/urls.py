fcfa742857efff4bce14908c7afbd6ce40f6472c
mgpo-98 <csi4613@naver.com>
2022-10-23 22:11:26 +0900

    1023

--- 장고연습/장고복습/accounts/urls.py
+++ 장고연습/장고복습/accounts/urls.py
@@ -1,7 +1,10 @@
-
+import imp
+from django.urls import path
+from . import views
 
 app_name = 'accounts'
  
 urlpatterns = [
-
+    path('signup/', views.signup, name='signup'),
+    path('<int:pk>/', views.detail, name='detail'),
 ]
--- 장고연습/장고복습/accounts/views.py
+++ 장고연습/장고복습/accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import CusetomUserCreationForm
+from . models import User
+from django.contrib.auth import get_user_model
+
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CusetomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    
+    else :
+        form = CusetomUserCreationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/signup.html', context)
+
+def detail(request,pk):
+   
+
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        'user': user
+    }
+
+    return render(request, 'accounts/detail.html')
