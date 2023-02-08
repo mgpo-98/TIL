5b82b32855b48b9239bba6359aa1c91f24f14b2e
mgpo-98 <csi4613@naver.com>
2023-02-08 17:43:57 +0900

    /

---
+++ 프로그래머스/키패드 누르기.py
@@ -0,0 +1,10 @@
+numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+hand = "right"
+answer = []
+for i in range(numbers):
+    if i in (1, 4, 7):
+        answer.append("L")
+    elif i in (3, 6, 9):
+        answer.append("R")
+    elif
+?
