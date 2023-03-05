13a314989ce596ed0473d69810a4543327f91faf
mgpo-98 <csi4613@naver.com>
2023-03-05 23:25:37 +0900

    0305

--- 백준/백준2/2012.py
+++ 백준/백준2/2012.py
@@ -2,12 +2,8 @@ n = int(input())
 a = []
 for i in range(n):
     a.append(int(input()))
-a.sort()    
+a.sort()
 c = 0
-for i in range(1,n+1):
-    c += abs(i-a[i-1])
+for i in range(1, n + 1):
+    c += abs(i - a[i - 1])
 print(c)
-
-
-
-.
