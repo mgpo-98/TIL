407e3a0a74661331c06fc4d28b2977298e03f0b1
mgpo-98 <csi4613@naver.com>
2023-04-18 13:29:49 +0900

    0418

--- javascript/은행계좌.js
+++ javascript/은행계좌.js
@@ -1,13 +1,17 @@
 class BankAccount {
   constructor(name, money) {
+    // 소유주
     this.holder = name;
+    // 잔액
     this.balance = money;
   }
 
+  // 입금하기
   deposit(money) {
     this.balance += money;
   }
 
+  // 출금하기
   withdraw(money) {
     if (this.balance - money < 0) {
       console.log('Insufficient balance');
@@ -16,6 +20,7 @@ class BankAccount {
     }
   }
 
+  // 이체하기
   transfer(money, anotherAccount) {
     const account = anotherAccount;
     if (this.balance - money < 0) {
